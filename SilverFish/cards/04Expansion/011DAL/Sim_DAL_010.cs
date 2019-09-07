d386874d41b9920772d39f77098d594cf0bc619e
ywtz1 <858030961@qq.com>
2019-09-07 10:28:14 +0800

    dal010

--- SilverFish/cards/04Expansion/011DAL/Sim_DAL_010.cs
+++ SilverFish/cards/04Expansion/011DAL/Sim_DAL_010.cs
@@ -6,7 +6,7 @@ namespace HREngine.Bots
 {
 	class Sim_DAL_010 : SimTemplate //* 托瓦格尔的阴谋
 	{
-		//在你使用一张鱼人牌后，随机将一张鱼人牌置入你的手牌
+		//
 		
         public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
         {
