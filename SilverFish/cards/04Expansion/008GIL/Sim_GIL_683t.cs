01e316b1965a07706678585e3f7879f6fc1e48c7
ywtz1 <858030961@qq.com>
2019-09-07 23:27:45 +0800

    合并jianjam GIL sim

---
+++ SilverFish/cards/04Expansion/008GIL/Sim_GIL_683t.cs
@@ -0,0 +1,16 @@
+using HREngine.Bots;
+
+namespace SilverFish.cards._04Expansion._008GIL
+{
+    /// <summary>
+    /// Drakeslayer
+    /// 飞龙猎手
+    /// </summary>
+    public class Sim_GIL_683t : SimTemplate
+    {
+        /// </summary>
+        // Poisonous
+        // 剧毒
+        /// </summary>
+    }
+}
