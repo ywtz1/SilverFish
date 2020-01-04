16d9b2eee83469d651673f899b5dc44c3fe0e479
ywtz1 <858030961@qq.com>
2020-01-04 10:03:30 +0800

    水晶学

--- SilverFish/cards/04Expansion/009BOT/Sim_BOT_909.cs
+++ SilverFish/cards/04Expansion/009BOT/Sim_BOT_909.cs
@@ -6,7 +6,7 @@ namespace HREngine.Bots
 {
 	class Sim_BOT_909 : SimTemplate //* 水晶学
 	{
-		// Battlecry: Give all minions in your hand +2/+2.
+		// 从你的牌库中抽两张攻击力为1的随从牌
             CardDB.Card c = null;       CardDB.Card c2 = null;  CardDB.Card c3 = null;    // 
         public override void onCardPlay(Playfield p, bool ownplay, Minion target, int choice)
         {
@@ -15,17 +15,18 @@ namespace HREngine.Bots
                 {
                     c = CardDB.Instance.getCardDataFromID(cn.Key);
 
-                    if (c.type == CardDB.cardtype.MOB&&p.restcard(cn.Key)>0)
+                    if (c.type == CardDB.cardtype.MOB&&p.restcard(cn.Key)>0&&c.Attack==1)
                     {
-                        if(c2==null){c2=c;continue;}
-                        if(c3==null){c3=c;continue;}
-                        if(c.cost < c2.cost){c3=c2;c2=c;}
+                        if(c2==null) c2=c;
+                        else if(c3==null) c3=c;
                         if(p.restcard(cn.Key)>1)c3=c2;
 
                     }
                 }
-                    p.drawACard(c2.cardIDenum, ownplay);
-                    p.drawACard(c3.cardIDenum, ownplay);
+                if(c2!=null)p.drawACard(c2.cardIDenum, ownplay);
+                if(c3!=null)p.drawACard(c3.cardIDenum, ownplay);
+                //p.drawACard(CardDB.cardName.unknown, ownplay);
+
 
         }
 	}
